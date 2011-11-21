db6431a6388d487e66dd1124bd6118e70b800624
Leif Eriksson <leif@leif.fi>
2011-11-21 19:49:51 +0200

    Changed build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-clear && ant debug && adb -s emulator-$1 install -r bin/Spuge-debug.apk
+clear && \
+    ant debug && \
+    adb -s emulator-$1 install -r bin/Spuge-debug.apk && \
+    adb -s emulator-$1 logcat
